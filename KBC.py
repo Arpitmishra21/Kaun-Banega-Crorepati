00ac399ba718eacbaec7129bcdc88c284662058c
Arpitmishra21 <125211783+Arpitmishra21@users.noreply.github.com>
2023-02-22 22:30:57 +0530

    Add files via upload

---
+++ KBC.py
@@ -0,0 +1,62 @@
+kbc=input("what is your name,  ")
+print('welcome to kaun banega corerepati',"powerd by \ndabar chavanprash","aaj humar sath hai",kbc ,"Jo ki nhi pta kaha se aaye hai","\nto aaye hum aapko jaldi se khel ki niyam bta de","\n1 khel me hum aapke same 5 prashan rakhenge jo ki 1 hazar se shuru ho kar 1 lakh rupye tak jainge","\n2 khel me aapko koi bhi life line nhi di jaigi agar aapko khelna hao to khele varna na khele","\n3 khel me aapke galat uttar dete hi khel ki samvti ho jaigi")
+# print("welcome to kaun banega corerepati",kbc)
+list1=["Who is the prime minister of india?","what is the capital of India","Who is the chief minister of chhattisgarh","4 Bhartiya Football team ke kaaptaan kaun hai","5 Who is the founder and Ceo Of CRED","6 Fastest Indian To Score 10000 Runs in International Cricket"]
+keyword1=["1 Narendra Modi","2 Amit Shah","3 MS Dhoni","4 Arvind Kejriwal"]
+keyword2=["1 Raipur" ,"2 Mumbai" ,"3 New Delhi" ,"4 Gandhinagar"]
+keyword3=["1 Bhupesh baghel ,2 Raman Singh,3 Brijmohan ,4 Rahul Gandhi"]
+keyword4=["1 Abhishek Singh","2 Sunil chetri","3 Manjit Roy","4 Neeraj Chopra"]
+keyword5=["1 Kunal Shah","Bhavish Aggrawal","Ritesh Aggrawal","Vijay Sharma"]
+keyword6=["1 Rohit Sharma","2 Sachin Tendulkar","3 Kumar Sangakara","4 Virat Kohli"]
+print(list1[0])
+print(keyword1)
+option=input("Answer")
+if(keyword1[0]):
+   print("congration you won 1000 rupees")
+else:
+  print("better luck next time")
+  breakpoint()
+print(list1[1])
+print(keyword2)
+kk=input("Ans")
+if(keyword2[2]):
+  print("congratulation You Won 2 Thousand rupees",kbc)
+else:
+  print("sorry manyavar ye galat uttar hai")
+  print(kbc,"aap aaj matra ek hazar rupye leke hi jainge")
+  breakpoint()
+print(list1[2])
+print(keyword3)
+ok=input("answer:")
+if(keyword3[0]):
+  print("Congratulation You Won 5 thousand rupees")
+else:
+  print("manyavar ye galat uttar hai")
+  print("aaj aap yaha se keval 2 hazar rupye hi jeet ke jainge")
+  breakpoint()
+print(list1[3])
+print(keyword4)
+bata=input("Answer De Jaldi")
+if(keyword4[1]):
+  print("mubarak ho aap jeet chuke hai 20 hazzar rupye")
+else:
+  print("shama kijyega ye uttar galat hai manyavar")
+  print("aaj aap keval 5 haazar rupye hi leke jainge")
+  breakpoint()
+print(list1[4])
+print(keyword5)
+kkd=input("Answer")
+if(keyword5[0]):
+   print("Mubarak Ho aap jeet chuke hai 1 lakh rupye")
+else:
+  print("manyavar ye galat uttar hai",'sahi uttar hai option A ratan naval Tata')
+  breakpoint()
+print(list1[5])
+print(keyword6)
+tkk=input()
+if(keyword6[3]):
+    print("mubarak ho aap jeet chuke hai 10 lakh rupye")
+else:
+    print("Manyavar ye uttar galat hai")
+    breakpoint()
+    
